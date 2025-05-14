ead66f0e9dc921211861c6e0547864983b47c841
Milton <gratno@gmail.com>
2025-05-13 21:37:09 -0500

    Actualización manual en README y mejoras en documentación

--- src/models/ProductosModel.py
+++ src/models/ProductosModel.py
@@ -12,6 +12,10 @@ class ProductosModel():
             with connection.cursor() as cursor:
                 cursor.execute("SELECT id_producto,categoria_producto,id_compra,caracteristicas_producto,tipo_producto,tamaño_producto,precio_producto,mes_del_producto,nombre_producto FROM productos ORDER BY nombre_producto ASC") 
                 resultset=cursor.fetchall()
+                
+                for row in resultset:
+                    productos=Productos()
+                
         except Exception as ex:
             return Exception(ex)    
         
