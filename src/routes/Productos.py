5f6e25a027b4091b6fac05f664c45c8f779c3959
Milton <gratno@gmail.com>
2025-05-14 21:00:35 -0500

    feat: agrega método get_producto y mejoras en modelo de productos

--- src/routes/Productos.py
+++ src/routes/Productos.py
@@ -12,12 +12,22 @@ main=Blueprint('productos_blueprint',__name__)
 def get_productos():
     
     try:
-        
         productos=ProductosModel.get_productos()
         return jsonify(productos)
         
     except Exception as ex:
         return jsonify({'massage': str(ex)}), 500 
-    
-    return jsonify({'message': "Vende_Productos"})
 
+@main.route('/<id_producto>')    
+def get_producto(id_producto):
+    
+    try:
+       producto=ProductosModel.get_producto(id_producto)
+       
+       if producto != None:
+           return jsonify(producto)
+       else:
+           return jsonify({}), 404
+        
+    except Exception as ex:
+        return jsonify({'massage': str(ex)}), 500 
