87a2686b6fb62ced94bcf50540f9e92bda5bcb8a
Milton <gratno@gmail.com>
2025-05-14 19:54:52 -0500

    Fix: productos ahora retorna JSON, ajustes en modelo y control de nulos

--- src/routes/Productos.py
+++ src/routes/Productos.py
@@ -1,9 +1,23 @@
 from flask import Blueprint
 from flask import jsonify
 
+#productos
+from models.ProductosModel import ProductosModel
+
+
+
 main=Blueprint('productos_blueprint',__name__)
 
 @main.route('/')
 def get_productos():
+    
+    try:
+        
+        productos=ProductosModel.get_productos()
+        return jsonify(productos)
+        
+    except Exception as ex:
+        return jsonify({'massage': str(ex)}), 500 
+    
     return jsonify({'message': "Vende_Productos"})
 
